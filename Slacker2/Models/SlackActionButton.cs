793a87d8859b383a4ff5f1376931e39b62ad9329
Park June Chul <pjc0247@naver.com>
2017-01-10 18:52:01 +0900

    update

---
+++ Slacker2/Models/SlackActionButton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Slacker2
+{
+	public class SlackActionButton
+	{
+		public string Name { get; set; }
+		public string Text { get; set; }
+
+	}
+}
---
+++ Slacker2/Models/SlackInteractiveMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slacker2
+{
+	public class SlackInteractiveMessage
+	{
+		public string Text { get; set; }
+		public string Description { get; set; }
+		public string CallbackId { get; set; }
+
+		public SlackActionButton[] Buttons { get; set; }
+	}
+}
