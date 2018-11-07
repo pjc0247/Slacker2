d2d764d3796a18c1c2731a5990e07f7f9fd172e4
Park June Chul <pjc0247@naver.com>
2018-11-07 12:45:21 +0900

    update

--- Slacker2/BotService.cs
+++ Slacker2/BotService.cs
@@ -9,14 +9,14 @@ namespace Slacker2
 	{
 		internal SlackService Slack { get; set; }
 
-		protected void AddReaction(SlackMessage message, string reactionName)
+		protected Task AddReaction(SlackMessage message, string reactionName)
 		{
 			reactionName = reactionName.Replace(":", "");
-			Slack.AddReaction(message.Channel.Id, message.Timestamp, reactionName);
+			return Slack.AddReaction(message.Channel.Id, message.Timestamp, reactionName);
 		}
-        protected void UpdateMessage(SlackMessage message, string messageToUpdate)
+        protected Task UpdateMessage(SlackMessage message, string messageToUpdate)
         {
-            Slack.UpdateMessage(message.Channel.Id, message.Timestamp, messageToUpdate);
+            return Slack.UpdateMessage(message.Channel.Id, message.Timestamp, messageToUpdate);
         }
 
 		protected Task<SlackMessage> SendColoredMessage(string channel, string message, string colorHex, string title, string description)
