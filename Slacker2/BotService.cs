1b6334be2ad436aa976d087c34f50eb566077e0a
Park June Chul <pjc0247@naver.com>
2018-11-07 14:16:56 +0900

    update

--- Slacker2/BotService.cs
+++ Slacker2/BotService.cs
@@ -9,39 +9,71 @@ namespace Slacker2
 	{
 		internal SlackService Slack { get; set; }
 
-		protected Task AddReaction(SlackMessage message, string reactionName)
+		protected Task AddReactionAsync(SlackMessage message, string reactionName)
 		{
 			reactionName = reactionName.Replace(":", "");
 			return Slack.AddReaction(message.Channel.Id, message.Timestamp, reactionName);
 		}
-        protected Task UpdateMessage(SlackMessage message, string messageToUpdate)
+        protected void AddReaction(SlackMessage message, string reactionName)
+        {
+            AddReactionAsync(message, reactionName);
+        }
+        protected Task UpdateMessageAsync(SlackMessage message, string messageToUpdate)
         {
             return Slack.UpdateMessage(message.Channel.Id, message.Timestamp, messageToUpdate);
         }
+        protected void UpdateMessage(SlackMessage message, string messageToUpdate)
+        {
+            UpdateMessageAsync(message, messageToUpdate);
+        }
 
-		protected Task<SlackMessage> SendColoredMessage(string channel, string message, string colorHex, string title, string description)
+        protected Task<SlackMessage> SendColoredMessageAsync(string channel, string message, string colorHex, string title, string description)
 		{
 			return Slack.SendColoredMessage(channel, message, colorHex, title, description);
 		}
-		protected Task<SlackMessage> SendColoredMessage(SlackChannel channel, string message, string colorHex, string title, string description)
+        protected void SendColoredMessage(string channel, string message, string colorHex, string title, string description)
+        {
+            SendColoredMessageAsync(channel, message, colorHex, title, description);
+        }
+
+        protected Task<SlackMessage> SendColoredMessageAsync(SlackChannel channel, string message, string colorHex, string title, string description)
 		{
 			return Slack.SendColoredMessage(channel.Name, message, colorHex, title, description);
 		}
+        protected void SendColoredMessage(SlackChannel channel, string message, string colorHex, string title, string description)
+        {
+            SendColoredMessageAsync(channel, message, colorHex, title, description);
+        }
 
-		protected Task<SlackMessage> SendMessage(string channel, string message)
+
+        protected Task<SlackMessage> SendMessageAsync(string channel, string message)
 		{
 			return Slack.SendMessage(channel, message);
 		}
-		protected Task<SlackMessage> SendMessage(SlackChannel channel, string message)
+        protected void SendMessage(string channel, string message)
+        {
+            SendMessageAsync(channel, message);
+        }
+
+		protected Task<SlackMessage> SendMessageAsync(SlackChannel channel, string message)
 		{
 			return Slack.SendMessage(channel.Name, message);
 		}
-		protected Task<SlackMessage> SendActionMessage(SlackChannel channel, string message, SlackInteractiveMessage messageData)
+        protected void SendMessage(SlackChannel channel, string message)
+        {
+            SendMessageAsync(channel, message);
+        }
+
+        protected Task<SlackMessage> SendActionMessageAsync(SlackChannel channel, string message, SlackInteractiveMessage messageData)
 		{
 			return Slack.SendActionMessage(channel.Name, message, messageData);
 		}
+        protected void SendActionMessage(SlackChannel channel, string message, SlackInteractiveMessage messageData)
+        {
+            SendActionMessageAsync(channel, message, messageData);
+        }
 
-		protected void GrantPermission(SlackUser user, string permission)
+        protected void GrantPermission(SlackUser user, string permission)
 		{
 			user.Permissions.Add(permission);
 		}
